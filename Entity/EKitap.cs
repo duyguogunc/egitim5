69e70b711b9fd271a2062d3eddefa03e24632ba1
Wissen <Wissen@WISSEN-PC>
2017-10-02 11:46:24 +0300

    E-Kitap Model

---
+++ Entity/EKitap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+   public class EKitap
+    {
+        [Key]
+        public int EKitapID { get; set; }
+        [Required]
+        [MaxLength(20, ErrorMessage ="Makale adı en fazla 20 karakter uzunluğunda olabilir.")]
+        [DisplayName("E-Kitap Adı")]
+        public string EKitapAdi { get; set; }
+        [Required]
+        [DisplayName("E-Kitap İçeriği")]
+        public string EKitapIcerik { get; set; }
+        [DisplayName("Eklenme Tarihi")]
+        public DateTime EklenmeTarihi { get; set; }
+        public EKitap()
+        {
+            EklenmeTarihi = DateTime.Now;
+        }
+    }
+}
