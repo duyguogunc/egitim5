0fba615bb17ce9cd3f0647064bbaa028e4fb6cf9
Wissen <Wissen@WISSEN-PC>
2017-10-02 11:51:36 +0300

    EKitap Model

--- Entity/EKitap.cs
+++ Entity/EKitap.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entity
 {
+    [Table("EKitaplar")]
    public class EKitap
     {
         [Key]
