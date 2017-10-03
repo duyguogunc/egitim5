b29c4d1dbb57dfde11820008154e8da11579f022
Wissen <Wissen@WISSEN-PC>
2017-10-03 13:37:16 +0300

    de

--- DAL/MyContext.cs
+++ DAL/MyContext.cs
@@ -1,4 +1,5 @@
 ﻿using Entity;
+using Entity.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
@@ -10,16 +11,23 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    public class MyContext :IdentityDbContext<Kullanici>
+
+
+
+
+    public class MyContext : IdentityDbContext<Kullanici>
     {
-        public MyContext():base("DefaultConnection")
+        public MyContext() : base("DefaultConnection")
         {
-                
+
         }
 
         public virtual DbSet<Video> Videolar { get; set; }
         public virtual DbSet<Makale> Makaleler { get; set; }
         public virtual DbSet<EKitap> EKitaplar { get; set; }
         public virtual DbSet<Kullanici> Kullanıcılar { get; set; }
+        public virtual DbSet<Konu> Konular { get; set; }
+
     }
 }
+
