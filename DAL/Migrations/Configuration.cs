cf69caf5c201eb05ff31e5dafeb13efd1710a64c
Wissen <Wissen@WISSEN-PC>
2017-10-03 10:10:02 +0300

    Kullanıcı Rolleri Tanımlandı

--- DAL/Migrations/Configuration.cs
+++ DAL/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace DAL.Migrations
 {
+    using Egitim5.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,18 +17,41 @@ namespace DAL.Migrations
 
         protected override void Seed(DAL.MyContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            //veritabanýnda kullanýcý yoksa
+            if (context.Users.Count() == 0)
+            {
+                #region kullaniciEkle
+                UserStore<ApplicationUser> str = new UserStore<ApplicationUser>(context);
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+                UserManager<ApplicationUser> mng = new UserManager<ApplicationUser>(str);
+
+                var admin = new ApplicationUser() { Email = "admin@yaz5.com", UserName = "admin@yaz5.com" };
+                var videomoderator = new ApplicationUser() { Email = "videomoderator@yaz5.com", UserName = "videomoderator@yaz5.com" };
+                var makalemoderator = new ApplicationUser() { Email = "makalemoderator@yaz5.com", UserName = "makalemoderator@yaz5.com" };
+                var ekitapmoderator = new ApplicationUser() { Email = "ekitapmoderator@yaz5.com", UserName = "ekitapmoderator@yaz5.com" };
+
+
+                mng.Create(admin, "Aa123456!"); //2. parametre þifresi
+                mng.Create(videomoderator, "Aa123456!"); 
+                mng.Create(makalemoderator, "Aa123456!"); 
+                mng.Create(ekitapmoderator, "Aa123456!"); 
+                #endregion
+
+                #region rolleriOlustur
+                context.Roles.Add(new IdentityRole() { Name = "Admin" });
+                context.Roles.Add(new IdentityRole() { Name = "VideoModerator" });
+                context.Roles.Add(new IdentityRole() { Name = "MakaleModerator" });
+                context.Roles.Add(new IdentityRole() { Name = "EKitapModerator" });
+                context.SaveChanges();
+                #endregion
+
+                #region kullanicilariRollereEkle
+                mng.AddToRole(admin.Id, "Admin");
+                mng.AddToRole(videomoderator.Id, "VideoModerator");
+                mng.AddToRole(makalemoderator.Id, "MakaleModerator");
+                mng.AddToRole(ekitapmoderator.Id, "EKitapModerator");
+                #endregion
+            }
         }
     }
 }
