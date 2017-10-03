d55be35f5b77befa588b3c2ca85eeaadb2b8708a
Wissen <Wissen@WISSEN-PC>
2017-10-03 13:29:41 +0300

    güncelleme

--- Business/Repository.cs
+++ Business/Repository.cs
@@ -1,4 +1,5 @@
 ﻿using Entity;
+using Entity.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +8,12 @@ using System.Threading.Tasks;
 
 namespace Business
 {
-  
-      public class VideoRep : BaseRepository<Video> { }
-      public class MakaleRep : BaseRepository<Makale> { }
-      public class EkitapRep : BaseRepository<EKitap> { }
-  
+
+    public class VideoRep : BaseRepository<Video> { }
+    public class MakaleRep : BaseRepository<Makale> { }
+    public class EkitapRep : BaseRepository<EKitap> { }
+    public class KonuRep : BaseRepository<Konu> { }
+
+
+
 }
