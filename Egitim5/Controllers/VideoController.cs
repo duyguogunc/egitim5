b74d8c58994efd9b5e517b0ee1a93cb0b98feb92
nuuklu <nuuklu@WISSEN-PC>
2017-12-05 19:12:18 +0300

    yıldız

--- Egitim5/Controllers/VideoController.cs
+++ Egitim5/Controllers/VideoController.cs
@@ -2,7 +2,6 @@
 using Entity;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,28 +11,42 @@ namespace Egitim5.Controllers
     public class VideoController : Controller
     {
         // GET: Video
-        public ActionResult Index()
+        public ActionResult Detail(int id)
         {
-            return View();
+            return View(new VideoRep().GetById(id));
         }
-        
-        public ActionResult VideoEkle()
+
+        public JsonResult VoteVideo(int id, int points)
         {
-            return View();
-        }
-        public ActionResult VideoDetay(string id)
-        {
-            var video = (from a in entity.Video where a.VideoID == GelenID select a).FirstOrDefault();
-            if (video != null)
-            {
-                ViewBag.Baslik = video.Baslik;
-                ViewBag.Icerik = video.Video1;
+            try
+            {//id için tanımlı olan Article kaydına points kadar puan ekleyelim  (TotalPoints)
+                if (Session["HasVoted_" + id] == null || (bool)Session["HasVoted_" + id] != true)
+                {
+                    VideoRep vRep = new VideoRep();
+                    Video selected = vRep.GetById(id);
+                    if (selected.TotalRate.HasValue)
+                        selected.TotalRate = selected.TotalRate.Value + points;
+                    else
+                        selected.TotalRate = points;
+                    vRep.Update(selected);
+                    Session["HasVoted_" + id] = true;
+
+                    return Json("Thank you for voting");
+                }
+                else
+                {
+                    return Json("you can't vote again!");
+                }
             }
-            return View();
+            catch (Exception ex)
+            {
+
+                return Json("A problem has occured - " + ex.Message);
+            }
         }
 
-    }
+
+
 
     }
-
-
+}
