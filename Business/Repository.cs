10a204e4028212719d049348b57aa2eedfde2cbd
Duygu Ogunc <duygu@egitim5.com>
2017-12-08 16:53:07 +0200

    fix

--- Business/Repository.cs
+++ Business/Repository.cs
@@ -2,15 +2,6 @@
 
 namespace Business
 {
-
-<<<<<<< HEAD
-    public class VideoRep : BaseRepository<Video> { }
-    public class MakaleRep : BaseRepository<Makale> { }
-    public class EkitapRep : BaseRepository<EKitap> { }
-    public class KonuRep : BaseRepository<Konu> { }
-
-    public class YorumRep : BaseRepository<Yorum> { }
-=======
     public class VideoRep : BaseRepository<Entity.Models.Video> { }
     public class MakaleRep : BaseRepository<Entity.Models.Makale> { }
     public class EkitapRep : BaseRepository<Entity.Models.EKitap> { }
@@ -18,7 +9,4 @@ namespace Business
     public class SikayetRep : BaseRepository<Entity.Models.Sikayet> { }
     public class OylamaRep : BaseRepository<Entity.Models.Oylama> { }
     
-  
->>>>>>> 364cedb898becf01efb4579e8ca09ab8e803274f
-
 }
--- DAL/MyContext.cs
+++ DAL/MyContext.cs
@@ -17,11 +17,8 @@ namespace DAL
         public virtual DbSet<EKitap> EKitaplar { get; set; }
         public virtual DbSet<Oylama> Oylamalar { get; set; }
         public virtual DbSet<Konu> Konular { get; set; }
-<<<<<<< HEAD
         public virtual DbSet<Yorum> Yorumlar { get; set; }
-=======
         public virtual DbSet<Sikayet> Sikayetler { get; set; }
->>>>>>> 364cedb898becf01efb4579e8ca09ab8e803274f
     }
 }
 
